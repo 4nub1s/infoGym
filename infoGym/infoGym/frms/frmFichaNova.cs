ca0f52f75c9b7f82483d5667ec2a0eaf2fe182eb
JC <joaocarlosfachini@gmail.com>
2014-03-07 20:01:02 -0300

    still working

--- infoGym/infoGym/frms/frmFichaNova.cs
+++ infoGym/infoGym/frms/frmFichaNova.cs
@@ -6,16 +6,23 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using infoGym.Classes.Negocio;
 
 namespace infoGym
 {
     public partial class frmFichaNova : Form
     {
         Usuario user;
+        Aluno aluno;
+        Ficha ficha;
+        bool isCadastrado;
         public frmFichaNova(Usuario user)
         {
             InitializeComponent();
+            isCadastrado = false;
             this.user = user;
+            this.aluno = new Aluno();
+            cbbObj.SelectedIndex = 0;
         }
 
         private void btnTreiAdd_Click(object sender, EventArgs e)
@@ -26,6 +33,51 @@ namespace infoGym
         private void btnTreiAtualizar_Click(object sender, EventArgs e)
         {
             new frmFichaItemTreino("").ShowDialog();
+        }
+
+        private void btnAluBuscar_Click(object sender, EventArgs e)
+        {
+            frmAluGerenciamento frmAlu = new frmAluGerenciamento(true, user);
+            frmAlu.ShowDialog();
+            if (frmAlu.getPesquisaRealizada())
+            {
+                aluno.Id = frmAlu.getAluId();
+                aluno = aluno.getAlunoById();
+                ttbAluno.Text = aluno.Nome;
+            }
+            else
+            {
+                aluno.Id = 0;
+                ttbAluno.Text = "";
+            }
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (ttbAluno.Text == "")
+                ttbAluno.BackColor = Color.Red;
+            else
+            {
+                ttbAluno.BackColor = Color.White;
+                ficha = new Ficha();
+                ficha.DtIni = dtpIni.Value.ToString();
+                ficha.DtIni = dtpFim.Value.ToString();
+                ficha.Finalidade = cbbObj.Text;
+                ficha.Status = "true";
+
+
+                isCadastrado = true;
+            }
+        }
+
+        private void tbTreinos_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tbTreinos_Enter(object sender, EventArgs e)
+        {
+       
         }
     }
 }
